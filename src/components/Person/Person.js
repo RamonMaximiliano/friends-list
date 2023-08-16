73594f16603c088d52cb454909b1b1b2151535dd
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2023-08-15 23:31:03 -0300

    updates

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,4 +1,16 @@
+import './styles.css';
 
 export const Person = () =>{
-    return <div>I'm person component </div>
+  const picture = "https://randomuser.me/api/portraits/thumb/men/65.jpg"
+
+    return (
+      <div class="person">
+          <img src={picture} alt="Profile picture" class="image"/>      
+          <p class="text">Name</p>
+          <p class="text">Job</p>
+      </div>
+    );
   };
+
+
+
