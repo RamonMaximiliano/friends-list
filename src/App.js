88fa24818e3fd4539d92a5ee3e453e6eb005c583
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2023-08-29 00:09:18 -0300

    updates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React from "react";
-import { PersonList } from './components/PersonList/PersonList'
-import './App.css';
+import React, { startTransition } from "react";
+import { PersonList } from "./components/PersonList/PersonList";
+import "./App.css";
+import { createContext } from "react";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
+export const MainContext = createContext("");
 
 const App = () => {
-  return <PersonList/>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PersonList />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
-
-
 
 /* 
 
@@ -20,6 +27,12 @@ https://www.youtube.com/watch?v=xuSoqDEkH90&list=PLnHJACx3NwAe5XQDk9xLgym7FF8Q4F
 Person image:
 https://randomuser.me/api/portraits/thumb/men/65.jpg
 
+
+- Create input component for the creation of people to then create the list of friends
+
+- Use two pages to practice context API, use a tag saying how many friends the list has, this will be the data shared between pages
+
+- Add a delete button to the profile of each
+
+
 */
-
-
